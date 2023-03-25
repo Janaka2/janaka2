3b53fe5cb02605b4fb642b7cacd6803abe4dcfef
Janaka Premathilaka <janaka2@gmail.com>
2023-03-25 16:28:40 +0100

    Create Profile.java

---
+++ Profile.java
@@ -0,0 +1,9 @@
+public class Profile {
+    private String name;
+    private String email;
+    private String position;
+    private List<String> skills;
+    private Map<String, String> socialLinks;
+
+    // Constructor, getters, and setters
+}
